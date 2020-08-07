1ea12d637084d8e1384e022f4a68d921f795aebd
ysaiascampos <ysaiascampos@gmail.com>
2020-08-06 21:55:56 -0400

    continuacion

---
+++ src/components/Formulario.js
@@ -0,0 +1,50 @@
+import React from 'react';
+const Formulario = () => {
+    return ( 
+        <div className="bg-info">
+            <div className="container">
+                <div className="row">
+                    <form
+                    className="col card text-white bg-transparent mb-5 pt-5 tb-2"
+                    >
+                        <fieldset>
+                            <legend className="text-center">
+                                Buscador Letras Canciones
+                            </legend>
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>Artista</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            neme="artista"
+                                            placeholder="Nombre Artista"
+                                        />    
+                                    </div>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>Canción</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            neme="cancion"
+                                            placeholder="Nombre Canción"
+                                        />    
+                                    </div>
+                                </div>
+                            </div>
+                            <button
+                                type="submit"
+                                className="btn btn-primary float-right"
+                            >Buscar</button>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default Formulario;
