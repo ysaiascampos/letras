8fb820116fa9d77bf3ce5acf3758ab277b2f44fa
ysaiascampos <ysaiascampos@gmail.com>
2020-08-13 21:00:50 -0400

    fin

--- src/components/Info.js
+++ src/components/Info.js
@@ -12,6 +12,17 @@ const Info = ({info}) => {
                 <p className="card-text">Género: {strGenre}</p>
                 <h2 className="card-text">Biografía:</h2>
                 <p className="card-text">{strBiographyES}</p>
+                <p className="card-text">
+                    <a href={`https://${info.strFacebook}`} target="_blank" rel="noopener noreferrer">
+                        <i className="fab fa-facebook"></i>
+                    </a>
+                    <a href={`https://${info.strTwitter}`} target="_blank" rel="noopener noreferrer">
+                        <i className="fab fa-twitter"></i>
+                    </a>
+                    <a href={`${info.strLastFMChart}`} target="_blank" rel="noopener noreferrer">
+                        <i className="fab fa-lastfm"></i>
+                    </a>
+                </p>
             </div>
         </div>
     )
